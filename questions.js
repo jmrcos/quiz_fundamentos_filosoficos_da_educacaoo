c72ad77fb2935ed1c91620628e9573b0b11bb62a
João Marcos <jmarcos106@gmail.com>
2025-04-07 21:01:38 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    question: "Quais eventos históricos foram acompanhados por uma rica produção filosófica?",
+    answers: [
+      { option: "A) A queda de Roma e a grande síntese filosófica de Santo Agostinho; o surgimento da filosofia escolástica na Idade Média; a Reforma Protestante, descobertas ultramarinas e o mercantilismo; o existencialismo após as guerras mundiais.", correct: true },
+      { option: "B) A Revolução Industrial, o iluminismo e o marxismo durante o século XX; o surgimento do capitalismo moderno e da escola de Frankfurt; as guerras mundiais e a economia mercantilista.", correct: false },
+      { option: "C) A Idade Média e o humanismo renascentista; a Revolução Científica e o positivismo; as revoluções industriais; as democracias modernas.", correct: false },
+    ],
+  },
+  {
+    question: "Qual foi o principal fator que contribuiu para o surgimento do pensamento filosófico na Grécia antiga?",
+    answers: [
+      { option: "A) A influência direta da filosofia persa e indiana.", correct: false },
+      { option: "B) A transformação socioeconômica da Grécia nos séculos VII e VI a.C., com o desenvolvimento da indústria artesanal e do comércio.", correct: true },
+      { option: "C) A falta de acesso a conhecimentos desenvolvidos por povos mais antigos, como os caldeus e os egípcios. D) O aumento das práticas religiosas na Grécia, vinculando o pensamento filosófico às explicações mitológicas.", correct: false },
+    ],
+  },
+  {
+    question: "Qual foi o principal objetivo da Academia, fundada por Platão??",
+    answers: [
+      { option: "A) Transmitir doutrinas éticas e políticas já estabelecidas.", correct: false },
+      { option: "B) Promover o ensino exclusivo da matemática e da lógica.", correct: false },
+      { option: "C) Ensinar o livre espírito de pesquisa e criar valores éticos e políticos a partir da reflexão.", correct: true },
+    ],
+  },
+  {
+    question: "De acordo com Platão, qual é o papel do filósofo no Estado ideal?",
+    answers: [
+      { option: "A) Desenvolver técnicas de persuasão para influenciar a população.", correct: false },
+      { option: "B) Controlar exclusivamente os aspectos econômicos do Estado.", correct: false },
+      { option: "C) Governar, zelando pelo bem espiritual e educando os cidadãos na virtude.", correct: true },
+    ],
+  },
+];
